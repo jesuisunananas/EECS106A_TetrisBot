6e2ffaf1157b4e660eed5d0d1ebb0879fc528175
Arjun Rewari <arjun.mahay@gmail.com>
2025-11-16 02:56:04 -0800

    commit

--- packing/packing.py
+++ packing/packing.py
@@ -22,13 +22,13 @@ def add_box(x, y, box):
     
 
 def can_add_box(x, y, box):
-    assert x >= 0
-    assert y >= 0
-    assert x + box.length <= bin.height_map.shape[1]
-    assert y + box.width <= bin.height_map.shape[0]
-    assert box.height + bin.height_map[x][y] <= bin.height
+    if x < 0: return False
+    if y< 0: return False
+    if x + box.length > bin.height_map.shape[1]: return False
+    if y + box.width > bin.height_map.shape[0]: return False
+    if box.height + bin.height_map[x][y] > bin.height: return False
     return True
- 
+
 b = Box(4,1,5)
 print(bin.height_map)
 add_box(0,0,b)
