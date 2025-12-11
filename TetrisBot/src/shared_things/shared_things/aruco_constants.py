f90e757369b572ae7e6c542075875148b4c60eea
Arjun Rewari <arjun.mahay@gmail.com>
2025-12-10 20:22:10 -0800

    removed resolution from bin in aruco constants, because it would result in a mismatch/bug

--- TetrisBot/src/shared_things/shared_things/aruco_constants.py
+++ TetrisBot/src/shared_things/shared_things/aruco_constants.py
@@ -23,7 +23,7 @@ BOXES = {
 
 BINS = {
     # 6: Bin(name='bag', length=10.0, width=10.0, height=1.0, id=6),
-    100: Bin(name='bin', length=0.3, width=0.3, height=1, id=100, resolution=0.02),
+    100: Bin(name='bin', length=0.3, width=0.3, height=1, id=100),
 }
 
 COLLISION_MESHES = {
